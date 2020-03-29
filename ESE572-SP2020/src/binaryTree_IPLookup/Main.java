044bb8d25eb68797d08b318dddb6002749ac8186
sxz294 <shenxiaozhi1993@hotmail.com>
2020-03-28 19:07:50 -0500

    ese 572 code

--- ESE572-SP2020/src/binaryTree_IPLookup/Main.java
+++ ESE572-SP2020/src/binaryTree_IPLookup/Main.java
@@ -11,6 +11,7 @@ public class Main {
 
 	
 	public static void main(String[] args) throws IOException {
+		System.out.println("");
 		// read prefix for building binary tree
 		BufferedReader br = new BufferedReader(
 				new InputStreamReader(
