2e9180c4f921aaed58589dcaf9b7556e8c60ce2b
sxz294 <shenxiaozhi1993@hotmail.com>
2020-03-28 19:12:07 -0500

    try commit

--- ESE572-SP2020/src/binaryTree_IPLookup/Main.java
+++ ESE572-SP2020/src/binaryTree_IPLookup/Main.java
@@ -11,7 +11,6 @@ public class Main {
 
 	
 	public static void main(String[] args) throws IOException {
-		System.out.println("");
 		// read prefix for building binary tree
 		BufferedReader br = new BufferedReader(
 				new InputStreamReader(
